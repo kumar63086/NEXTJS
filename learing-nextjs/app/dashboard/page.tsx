6afb19ba63eeeb134c4ed1abada4515edbadae1a
kumar866 <kumarborelli0@gmail.com>
2025-10-18 16:28:43 +0530

    first commit

---
+++ learing-nextjs/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardRouter() {
+  return (
+    <h1>Hello, this is Dashboard Page</h1>
+  );
+}
---
+++ learing-nextjs/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <><h1>hellow from this is index page </h1>
+    
+    </>
+   
+
+  );
+}
