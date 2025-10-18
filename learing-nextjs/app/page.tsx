1918ebf3a078f661e55d5da030e06baf5af1e981
kumar866 <kumarborelli0@gmail.com>
2025-10-18 16:58:17 +0530

    this is navigation

--- learing-nextjs/app/page.tsx
+++ learing-nextjs/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <><h1>hellow from this is index page </h1>
-    
-    </>
-   
+    <div className="flex items-center"><h1>hellow from this is index page </h1>
+      <div>
+
+        <Link href="/dashboard" className="underline">dashboard</Link> 
+        <br/>
+        <Link href="/home" className="underline text-purple-600 " >home</Link>   
+        </div>
+    </div>
+
 
   );
 }
